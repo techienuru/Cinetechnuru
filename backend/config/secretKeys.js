e6705cbb0f4c315336bf092a2e02ba77aca35937
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-07-10 17:00:45 +0100

    Add Secret key for TMDB API, and Change "npm start" to "npm run dev"

--- backend/config/secretKeys.js
+++ backend/config/secretKeys.js
@@ -2,5 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { DATABASE_URI, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } =
-  process.env;
+export const {
+  DATABASE_URI,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+  TMDB_API_KEY,
+  TMDB_ACCESS_TOKEN,
+} = process.env;
